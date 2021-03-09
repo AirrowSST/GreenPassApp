0a82738605d78b1574b7b6791df3150679957015
Yaw Tia <airrowsst@gmail.com>
2021-03-09 11:42:43 +0800

    old

--- app/src/main/java/com/example/greenpassapp/model/FakeController.java
+++ app/src/main/java/com/example/greenpassapp/model/FakeController.java
@@ -1,7 +1,11 @@
 package com.example.greenpassapp.model;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class FakeController {
     public static void main(String[] args) {
         System.out.println("old");
     }
+
+    // fdkjnfkjnfdj
 }
